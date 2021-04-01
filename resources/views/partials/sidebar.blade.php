235d18d09abb670588189d42683ea5526da5a4c5
Yanuardi <52162301+zulhwk@users.noreply.github.com>
2021-04-01 17:32:47 +0700

    changes location files image

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-white border-r min-h-screen"> 
     <div class="flex items-center">
         <a href="{{ route('admin.dashboard') }}" class="flex items-center mt-2">
-            <img class="h-12 block ml-6 w-12" src="{{ asset('vendor/avored/images/logo_only.svg') }}" />
+            <img class="h-12 block ml-6 w-12" src="{{ asset('avored-admin/images/logo_only.svg') }}" />
             <div class="text-xl text-red-600" :class="sidebar ? 'hidden' : ''">AvoRed</div>
         </a>
     </div>
